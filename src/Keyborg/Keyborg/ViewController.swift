d1887aaadf78fe658688f2ca189fd0b4b811f024
bono <bornthenord@gmail.com>
2025-04-14 11:22:42 +0300

    init

---
+++ src/Keyborg/Keyborg/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Keyborg
+//
+//  Created by cat dog on 14.04.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

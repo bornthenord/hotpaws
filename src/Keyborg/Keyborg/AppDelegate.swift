0dc8b7974492f422a5a310bc773a1531c02a829d
bono <bornthenord@gmail.com>
2025-04-14 15:24:16 +0300

    menu bar

--- src/Keyborg/Keyborg/AppDelegate.swift
+++ src/Keyborg/Keyborg/AppDelegate.swift
@@ -10,8 +10,9 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     var keyborg: Keyborg?
-    
-
+    var statusItem: NSStatusItem?
+    @IBOutlet weak var menu: NSMenu?
+    @IBOutlet weak var firstMenuItem: NSMenuItem?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         keyborg = Keyborg()
@@ -24,7 +25,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
 
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem?.menu = menu
+        
+        statusItem?.button?.title = "keyborg"
+//        statusItem?.button?.image = NSImage(systemSymbolName: "hand.raised.square.on.square", accessibilityDescription: nil)
+        
+        // The application is an ordinary app that appears in the Dock and may
+        // have a user interface.
+        NSApp.setActivationPolicy(.regular)
 
+        // The application does not appear in the Dock and does not have a menu
+        // bar, but it may be activated programmatically or by clicking on one
+        // of its windows.
+        NSApp.setActivationPolicy(.accessory)
+
+        // The application does not appear in the Dock and may not create
+        // windows or be activated.
+        NSApp.setActivationPolicy(.prohibited)
+    }
 }
 
